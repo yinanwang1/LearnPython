945d6395b9d8596ad25685c9c707c9fd475cf7d3
ArthurWang <wangyn@qeek.cc>
2021-11-04 11:28:50 +0800

    update

---
+++ LeetCode6/containsDuplicate.py
@@ -0,0 +1,12 @@
+
+# 给定一个整数数组，判断是否存在重复元素。
+#
+# 如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+
+
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
