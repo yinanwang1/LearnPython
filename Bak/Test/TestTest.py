2b85371080bb54b4410b58a0d362be280a7152fc
Arthur <wangyn@qeek.cc>
2025-12-19 14:57:57 +0800

    update

--- Bak/Test/TestTest.py
+++ Bak/Test/TestTest.py
@@ -8,3 +8,11 @@ def tag(name):
 
 with tag("h1"):
     print("This is Ttitle")
+
+
+
+# Python 像JS一样支持一个变量多种类型
+class ListNode:
+    def __init__(self, val: int):
+        self.val: int = val
+        self.next: ListNode | None = None
