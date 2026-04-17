f5e74945d79dfd43aa9dab6210bd9b9d32996090
Arthur <wangyn@qeek.cc>
2026-04-17 13:21:01 +0800

    新增算法

---
+++ Bak/LeetCode10/minMirrorPairDistance.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from streamlit.elements import empty
+
+
+class Solution:
+    def minMirrorPairDistance(self, nums: List[int]) -> int:
+        pre_dict = {}
+        res = len(nums)
+        for i, num in enumerate(nums):
+            if num in pre_dict:
+                res = min(res, i - pre_dict[num])
+            pre_dict[int(str(num)[::-1])] = i
+        return res if res < len(nums) else -1
+
+
+
+
+
+if __name__ == '__main__':
+    # print(Solution().minMirrorPairDistance([1, 2, 3, 4, 5]))
+    # print(Solution().minMirrorPairDistance([9,9]))
