975385d3b2f22ffe839467b2faa7216c3efd258b
Arthur <wangyn@qeek.cc>
2026-05-19 10:08:48 +0800

    新增算法  2540. 最小公共值

---
+++ LeetCode/LeetCode11/getCommon.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+# 2540. 最小公共值
+# https://leetcode.cn/problems/minimum-common-value/description/
+
+
+
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        left = 0
+        right = 0
+        while left < len(nums1) and right < len(nums2):
+            if nums1[left] == nums2[right]:
+                return nums1[left]
+            elif nums1[left] < nums2[right]:
+                left += 1
+            else:
+                right += 1
+
+        return -1
