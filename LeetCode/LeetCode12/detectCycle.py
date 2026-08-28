3a10125e8e5c2efe9505ea12f5c756adea66233c
Arthur <wangyn@qeek.cc>
2026-08-28 15:09:39 +0800

    新增算法，快慢指针，好方法啊。

---
+++ LeetCode/LeetCode12/detectCycle.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        nodes = set()
+        while head:
+            if head in nodes:
+                return head
+            else:
+                nodes.add(head)
+            head = head.next
+        else:
+            return None
+
