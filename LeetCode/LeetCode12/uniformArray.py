43349fd16636707936c7194532df11531cdecb39
Arthur <wangyn@qeek.cc>
2026-09-02 10:09:30 +0800

    新增算法。 直接返回 True的答案，真的是第一次看到。

---
+++ LeetCode/LeetCode12/uniformArray.py
@@ -0,0 +1,7 @@
+# 3875. 构造奇偶一致的数组 I
+# https://leetcode.cn/problems/construct-uniform-parity-array-i/description/
+
+
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
